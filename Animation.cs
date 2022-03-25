a5878691f75488b9d63a07afab57e92791d1bea4
MrH115 <92683158+MrH115@users.noreply.github.com>
2022-03-25 12:37:14 +0300

    Add project files.

---
+++ Animation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FonttyAnimation
+{
+    public class Animation
+    {
+        public int width, height, Xpos, Ypos, frames, currentFrame;
+        public Rectangle rectangle;
+        public Texture2D texture;
+        public Vector2 origin, position;
+        public float frameTime, timer;
+        public bool isLooped, isFliped;
+
+        public Animation(Texture2D texture, int width, int height,int Xpos,int Ypos,int frames, float timer, Vector2 pos, bool isLooped, bool isFliped)
+        {
+            this.texture = texture;
+            this.width = width;
+            this.height = height;
+            this.Xpos = Xpos;
+            this.Ypos = Ypos;
+            frameTime = timer;
+            position = pos;
+            origin = new Vector2(width / 2, height / 2);
+            rectangle = new Rectangle(Xpos, Ypos, width, height);
+            this.frames = frames;
+            this.timer = 0;
+            currentFrame = 0;
+            this.isLooped = isLooped;
+            this.isFliped = isFliped;
+        }
+        public void Update(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            rectangle = new Rectangle(Xpos + (width * currentFrame), Ypos, width, height);
+            if (timer > frameTime)
+            {
+                if (currentFrame < frames)
+                {
+                    currentFrame++;
+                    timer = 0;
+                }
+                else if (currentFrame >= frames && isLooped)
+                {
+                    currentFrame = 0;
+                    timer = 0;
+                }
+            }
+        }
+        public void Draw(SpriteBatch sp)
+        {
+            if(!isFliped)
+                sp.Draw(texture, position, rectangle, Color.White, 0, origin,1, SpriteEffects.None, 0);
+            else if(isFliped)
+                sp.Draw(texture, position, rectangle, Color.White, 0, origin, 1, SpriteEffects.FlipHorizontally, 0);
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FonttyAnimation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        public Texture2D sheet1;
+        public Texture2D sheet2;
+        public Texture2D sheet3;
+        public Animation animation;
+        public Vector2 position = new Vector2(75, 75);
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            sheet1 = Content.Load<Texture2D>("MiniReaper");
+            sheet2 = Content.Load<Texture2D>("MiniZombie");
+            sheet3 = Content.Load<Texture2D>("MiniPrinceMan");
+            animation = new Animation(sheet3, 32, 32, 0, 32, 5, 0.3f, position, true, true);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            animation.Update(gameTime);
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                animation.isFliped = false;
+                animation.position.X += 0.3f;
+            }
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                animation.position.X -= 0.3f;
+                animation.isFliped = true;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DeepSkyBlue);
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, Matrix.CreateScale(3f));
+            animation.Draw(_spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
